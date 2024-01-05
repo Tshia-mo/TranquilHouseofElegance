027960248250081342fc7fd0264c287f6403c2c0
Tshiamoramong <122871838+Tshia-mo@users.noreply.github.com>
2024-01-05 22:57:08 +0200

    uncomplete

---
+++ src/index.js
@@ -0,0 +1,8 @@
+function toggleSubpage() {
+  var subpage = document.getElementById("subpage");
+  if (subpage.style.display === "none") {
+    subpage.style.display = "block";
+  } else {
+    subpage.style.display = "none";
+  }
+}
