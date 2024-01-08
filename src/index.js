0cf7806ed7138acd19b97e4b01b766974f197f11
Tshiamoramong <122871838+Tshia-mo@users.noreply.github.com>
2024-01-08 03:54:28 +0200

    added container

--- src/index.js
+++ src/index.js
@@ -6,3 +6,33 @@ function toggleSubpage() {
     subpage.style.display = "none";
   }
 }
+
+function submitForm() {
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var subject = document.getElementById("subject").value;
+  var message = document.getElementById("message").value;
+
+  var phoneNumber = "+27836074964";
+  var messageText =
+    "New form submission:\n\nName: " +
+    name +
+    "\nEmail: " +
+    email +
+    "\nSubject: " +
+    subject +
+    "\nMessage: " +
+    message;
+  alert("Form submitted successfully!\n\n" + messageText);
+
+  fetch("/submit", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name, email, subject, message }),
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.error("Error:", error));
+}
