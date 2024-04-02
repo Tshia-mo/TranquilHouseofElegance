74f706c9f5c39c48205dfa6ff96c5c5d6b47c9c3
Tshiamoramong <122871838+Tshia-mo@users.noreply.github.com>
2024-04-02 16:23:51 +0200

    almost done

--- src/index.js
+++ src/index.js
@@ -1,38 +1,9 @@
-function toggleSubpage() {
-  var subpage = document.getElementById("subpage");
-  if (subpage.style.display === "none") {
-    subpage.style.display = "block";
-  } else {
-    subpage.style.display = "none";
-  }
-}
+document.addEventListener("DOMContentLoaded", function () {
+  var form = document.getElementById("contact-form");
 
-function submitForm() {
-  var name = document.getElementById("name").value;
-  var email = document.getElementById("email").value;
-  var subject = document.getElementById("subject").value;
-  var message = document.getElementById("message").value;
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
 
-  var phoneNumber = "+27836074964";
-  var messageText =
-    "New form submission:\n\nName: " +
-    name +
-    "\nEmail: " +
-    email +
-    "\nSubject: " +
-    subject +
-    "\nMessage: " +
-    message;
-  alert("Form submitted successfully!\n\n" + messageText);
-
-  fetch("/submit", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ name, email, subject, message }),
-  })
-    .then((response) => response.json())
-    .then((data) => console.log(data))
-    .catch((error) => console.error("Error:", error));
-}
+    form.reset();
+  });
+});
